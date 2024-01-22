0b0aa9f59c1ae313475e577d43e9a1a307944193
haechan <haechan2000@noutecompany.com>
2024-01-23 03:18:14 +0900

    feat: webView를 화면에 띄우는 코드추가

--- lib/screen/home_screen_2.dart
+++ lib/screen/home_screen_2.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
+final homeUrl = Uri.parse('https://yhchan2000.notion.site/6-9f91306e3f794dd98db109400e9a6b4a?pvs=4');
+
 class HomeScreen2 extends StatelessWidget {
-  WebViewController controller = WebViewController();
+  WebViewController controller = WebViewController()..loadRequest(homeUrl); // .. 점 두개: 함수를 실행한 대상을 반환하라
+
   HomeScreen2({super.key});
 
   @override
@@ -10,7 +13,7 @@ class HomeScreen2 extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.green,
-        title: const Text('Code Factory'),
+        title: const Text('Yang'),
         centerTitle: true,
       ),
       body: WebViewWidget(
