5a9fec1b129ac852b045f668c6d09f9e92038f4e
haechan <haechan2000@noutecompany.com>
2024-01-23 03:35:20 +0900

    feat: 자바스크립트 사용을 위한 setJavaScriptMode 설정

--- lib/screen/home_screen_2.dart
+++ lib/screen/home_screen_2.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-final homeUrl = Uri.parse('https://yhchan2000.notion.site/6-9f91306e3f794dd98db109400e9a6b4a?pvs=4');
+final homeUrl = Uri.parse(
+    'https://yhchan2000.notion.site/6-9f91306e3f794dd98db109400e9a6b4a?pvs=4');
 
 class HomeScreen2 extends StatelessWidget {
-  WebViewController controller = WebViewController()..loadRequest(homeUrl); // .. 점 두개: 함수를 실행한 대상을 반환하라
+  WebViewController controller = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted) // 안드로이드는 자바스크립트 사용에 대한 세팅을 따로 해줘야 된다
+    ..loadRequest(homeUrl); // .. 점 두개: 함수를 실행한 대상을 반환하라
 
   HomeScreen2({super.key});
 
@@ -15,6 +18,16 @@ class HomeScreen2 extends StatelessWidget {
         backgroundColor: Colors.green,
         title: const Text('Yang'),
         centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              controller.loadRequest(homeUrl);
+            },
+            icon: Icon(
+              Icons.home,
+            ),
+          ),
+        ],
       ),
       body: WebViewWidget(
         controller: controller,
