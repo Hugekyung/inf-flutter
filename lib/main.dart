e407e3cee409cb2f2e801f1f2f4dbaea64157864
haechan <haechan2000@noutecompany.com>
2024-02-12 03:08:30 +0900

    feat: TopPart, BottomPart 클래스 분리

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:test_project/screen/Home_screen_uandi.dart';
+import 'package:test_project/screen/home_screen_uandi.dart';
 
 void main() {
   // Flutter 프레임워크가 실행할 준비가 될때까지 기다린다
---
+++ lib/screen/home_screen_uandi.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class HomeScreenUandI extends StatefulWidget {
+  const HomeScreenUandI({super.key});
+
+  @override
+  State<HomeScreenUandI> createState() => _HomeScreenUandIState();
+}
+
+class _HomeScreenUandIState extends State<HomeScreenUandI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.blue[300],
+        body: SafeArea(
+          bottom: false,
+          child: Container(
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              children: [
+                _TopPart(),
+                _BottomPart(),
+              ],
+            ),
+          ),
+        ));
+  }
+}
+
+class _TopPart extends StatelessWidget {
+  const _TopPart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(
+            'U&I',
+            style: TextStyle(
+              color: Colors.white,
+              fontFamily: 'single',
+              fontSize: 100.0,
+            ),
+          ),
+          Column(
+            children: [
+              Text(
+                '우리 처음 만난 날',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'single',
+                  fontSize: 50.0,
+                ),
+              ),
+              Text(
+                '2022.12.10',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'dongle',
+                    fontSize: 35.0,
+                    fontWeight: FontWeight.normal),
+              ),
+            ],
+          ),
+          IconButton(
+            iconSize: 60.0,
+            onPressed: () {},
+            icon: Icon(
+              Icons.favorite,
+              color: Colors.pink[300],
+            ),
+          ),
+          Text(
+            'D+1',
+            style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'dongle',
+                fontSize: 70.0,
+                fontWeight: FontWeight.bold),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _BottomPart extends StatelessWidget {
+  const _BottomPart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Image.asset(
+        'asset/img/mountain.jpg',
+      ),
+    );
+  }
+}
